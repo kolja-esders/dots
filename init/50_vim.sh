f90f3480edf82152e170f7da27bb4cbe915b74a8
Kolja Esders <kolja.esders@gmail.com>
2017-02-23 02:09:02 +0100

    Installing neovim on ubuntu

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,6 +1,9 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
   vim +PlugUpgrade +PlugUpdate +qall
