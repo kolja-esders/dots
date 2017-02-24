986d0c40c98b3a4203e97e4574df32789bb973cb
Kolja Esders <kolja.esders@gmail.com>
2017-02-24 01:40:13 +0100

    Loading plug for vim

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,8 +1,10 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -d "~/.local/share/nvim/site/autoload/plug.vim" ]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
