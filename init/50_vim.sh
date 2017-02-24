8e0370af9ef747e5b9ee2fd0e0394c0734aeffe7
Kolja Esders <kolja.esders@gmail.com>
2017-02-24 02:04:48 +0100

    No longer needed to install vim-plug

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,12 +1,12 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
 
-if [ ! -d "~/.local/share/nvim/site/autoload/plug.vim" ]; then
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+#if [ ! -d "~/.local/share/nvim/site/autoload/plug.vim" ]; then
+#    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#fi
 
 # Download Vim plugins.
-if [[ "$(type -P vim)" ]]; then
-  vim +PlugUpgrade +PlugUpdate +qall
-fi
+#if [[ "$(type -P vim)" ]]; then
+#  vim +PlugUpgrade +PlugUpdate +qall
+#fi
